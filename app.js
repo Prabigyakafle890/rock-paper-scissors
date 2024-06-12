338b6f5c00a116638fe91282e7d8aaff354b3dd9
Prabigya Kafle <prabigyakafle50@gmail.com>
2024-06-12 17:11:44 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+let userScore=0;
+let compScore=0;
+let result= document.getElementById("result");
+
+const genCompChoice = () => {
+    const options=["rock", "paper", "scissors"];
+    const RandIdX=Math.floor(Math.random()*3);
+    return options[RandIdX];
+}
+
+const playGame=(userChoice)=>{
+
+ const compChoice= genCompChoice();
+ console.log("computer choice=",compChoice)
+ console.log("user choice=",userChoice);
+
+ if(userChoice === compChoice){
+   
+drawGame();
+ }
+ else{
+    let userWin=true;
+    if(userChoice==="rock"){
+userWin = compChoice==="paper"? false: true;
+
+    }else if(userChoice==="paper"){
+        userWin = compChoice==="scissors" ? false: true;
+    }
+    else{
+        userWin = compChoice==="rock" ? false: true;
+    }
+    scoreCalculator(userWin);
+ }
+
+ 
+
+};
+
+const drawGame=()=>{
+    userScore=userScore;
+    compScore=compScore;
+      result.innerHTML="Game was draw."
+    console.log("Game was draw")
+}
+
+const scoreCalculator =(userWin,compChoice,userChoice)=>{
+    if(userWin== true){
+        userScore+=1;
+result.innerHTML="You won the game."
+        document.getElementById('user-score').innerHTML=userScore;
+        document.getElementById('comp-score').innerHTML=compScore;
+        console.log("user score=",userScore)
+        console.log("Computer score=",compScore)
+    }
+    else {
+        compScore+=1;
+        result.innerHTML="Computer won the game."
+        document.getElementById('user-score').innerHTML=userScore;
+        document.getElementById('comp-score').innerHTML=compScore;
+        console.log("Computer score=",compScore)
+        console.log("user score=",userScore)
+    }
+}
+
+const choices= document.querySelectorAll('.choice');
+
+choices.forEach((choice)=>{
+choice.addEventListener("click",()=>{
+
+    const userChoice=choice.getAttribute('Id')
+    playGame(userChoice);
+   
+});
+
+});
